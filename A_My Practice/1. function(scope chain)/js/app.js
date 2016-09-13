09c9bc356f44c3796a411b42e4036daff8005392
timur <timur@timur.local>
2016-09-13 18:15:36 -0400

    scope chain practice

---
+++ A_My Practice/1. function(scope chain)/js/app.js
@@ -0,0 +1,21 @@
+function fn_1(x) {
+	
+	
+		function fn_2(y) {
+			return y;
+		}
+	
+
+		
+	var phone = "647-629-2765";
+
+	var run = fn_2(phone);
+	console.log(run + ' Inside Function - Scope Chain');
+
+	return x;
+}
+
+var phone = "905-761-9999";
+var run = fn_1(phone);
+console.log(this.run + ' Global Execution Context');
+
