b23c9ea3ded1fd5aeaa20c59de460c4f78e26ed0
timur <timur@timur.local>
2016-09-16 22:06:13 -0400

    coercion eg

--- A_My Practice/1. function(scope chain)/js/app.js
+++ A_My Practice/1. function(scope chain)/js/app.js
@@ -1,21 +1,16 @@
 function fn_1(x) {
-	
-	
-		function fn_2(y) {
-			return y;
-		}
-	
-
-		
-	var phone = "647-629-2765";
-
-	var run = fn_2(phone);
-	console.log(run + ' Inside Function - Scope Chain');
-
-	return x;
+    function fn_2(x) {
+        return x;
+    }
+    var phone = "647-629-2765";
+    var run = fn_2(phone);
+    console.log(run + ' Inside Function - Scope Chain');
+    return x;
 }
-
 var phone = "905-761-9999";
 var run = fn_1(phone);
 console.log(this.run + ' Global Execution Context');
 
+var tata= function wendy(){
+	console.log('hi')
+};
